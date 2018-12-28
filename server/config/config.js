462cc4b5aa553abf067fe88c2be9c6bc7ea98c77
HazaelJDev <jejibarrera@gmail.com>
2018-12-27 23:58:26 -0600

    RestServer - usario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,22 @@
 //                 PORT
 // ####################################
 process.env.PORT = process.env.PORT || 3000;
+
+// ####################################
+//                 Entorno
+// ####################################
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ####################################
+//                 Data Base
+// ####################################
+let urlDB;
+if (process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafeteria';
+}else{
+    urlDB = 'mongodb://cafe-admin:L4D7aN@ds245234.mlab.com:45234/cafeteria';
+}
+
+process.env.URLDB = urlDB;
+
+
--- server/server.js
+++ server/server.js
@@ -2,41 +2,22 @@ require('./config/config');
 const express =  require('express');
 const app =  express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 //parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended:false}));
 //parse application/json
 app.use(bodyParser.json());
+//Rutas
+app.use(require('./routes/usuario'));
 
-app.get('/usuario',(req,res) => {
-    res.json('get Usuario');
+//Conexion BD
+mongoose.connect(process.env.URLDB,(err,res)=>{
+    if(err) throw err;
+    console.log('Base de dastos online');
 });
 
-app.post('/usuario',(req,res) => {
-    let body = req.body;
-    if(body.persona == undefined){
-        res.status(400).json({
-            ok: false,
-            message: 'Es necesario el nombre'
-        });
-    }else{
-        res.json({
-            persona: body
-        });
-    }
 
-});
-
-app.put('/usuario/:id',(req,res) => {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario/:id',(req,res) => {
-    res.json('delete Usuario');
-});
 
 app.listen(process.env.PORT,()=>{
     console.log(`Servidor escuchando en puerto ${process.env.PORT}`);
